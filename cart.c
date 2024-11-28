2429ae0dfaec14122c56d70fa37f0222711f46f2
David Konsumer <konsumer@jetboystudio.com>
2024-11-28 01:07:23 -0800

    cleanup

--- cart.c
+++ cart.c
@@ -1,16 +1,18 @@
 #include <stdint.h>
 
+// this import comes from host wasm
 __attribute__((import_module("null0"), import_name("host_function_that_takes_string_param")))
 void host_function_that_takes_string_param(char* text);
 
+// this is called externally, in host, to test if the memory is modified
 __attribute__((export_name("cartFunction")))
 void cartFunction(int32_t* data, size_t length) {
-    // Example function that manipulates shared memory
     for (size_t i = 0; i < length; i++) {
         data[i] += 1;
     }
 }
 
+// this is called externally, in host, to test string
 __attribute__((export_name("send_request_to_host")))
 void send_request_to_host() {
   host_function_that_takes_string_param("Cool!");
--- host.c
+++ host.c
@@ -2,14 +2,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
+// this is called externally, in host, to test if the memory is modified
 EMSCRIPTEN_KEEPALIVE void hostFunction(int32_t* data, size_t length) {
-  // Example function that manipulates shared memory
   for (size_t i = 0; i < length; i++) {
       data[i] *= 2;
   }
 }
 
-
+// this is called by cart directly (from host-binding)
 EMSCRIPTEN_KEEPALIVE void host_function_that_takes_string_param(char* text) {
   printf("HOST: cart sent me %s\n", text);
 }

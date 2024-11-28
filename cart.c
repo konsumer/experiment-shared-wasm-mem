5cbac5076556be8797227184ec7071c144ccd0c9
David Konsumer <konsumer@jetboystudio.com>
2024-11-28 00:44:43 -0800

    add string test

--- cart.c
+++ cart.c
@@ -1,9 +1,17 @@
 #include <stdint.h>
 
+__attribute__((import_module("null0"), import_name("host_function_that_takes_string_param")))
+void host_function_that_takes_string_param(char* text);
+
 __attribute__((export_name("cartFunction")))
 void cartFunction(int32_t* data, size_t length) {
     // Example function that manipulates shared memory
     for (size_t i = 0; i < length; i++) {
         data[i] += 1;
     }
+}
+
+__attribute__((export_name("send_request_to_host")))
+void send_request_to_host() {
+  host_function_that_takes_string_param("Cool!");
 }
--- host.c
+++ host.c
@@ -1,9 +1,15 @@
 #include <emscripten.h>
 #include <stdint.h>
+#include <stdio.h>
 
 EMSCRIPTEN_KEEPALIVE void hostFunction(int32_t* data, size_t length) {
   // Example function that manipulates shared memory
   for (size_t i = 0; i < length; i++) {
       data[i] *= 2;
   }
+}
+
+
+EMSCRIPTEN_KEEPALIVE void host_function_that_takes_string_param(char* text) {
+  printf("HOST: cart sent me %s\n", text);
 }
